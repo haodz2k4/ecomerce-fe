4d0e51d9361b4c7e1af9753fa96b813174e8f4fb
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-02-24 14:12:18 +0700

    Config axios instance

---
+++ src/api/axios.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:4000/api/v1/',
+    timeout: 10000
+})
+
+axiosInstance.interceptors.request.use(function (config) {
+
+    return config;
+}, function (error) {
+    return Promise.reject(error);
+})
+
+axiosInstance.interceptors.response.use(function (res) {
+    return res
+}, function (error) {
+    return Promise.reject(error);
+})
+
+export default axiosInstance
