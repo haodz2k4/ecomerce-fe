cf4444305513da6870d61cd28fbc2380bf376cfb
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-02-27 17:12:27 +0700

    Integration NProgress

--- src/modules/client/components/layouts/Main/Main.layout.tsx
+++ src/modules/client/components/layouts/Main/Main.layout.tsx
@@ -1,10 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import Header from "../../ui/Header/Header"
 import Footer from "../../ui/Footer/Footer"
+import { useEffect } from "react";
+import NProgress from "../../../../../config/nprogress";
 
 
 export const MainLayout = () => {
 
+    const location = useLocation();
+    useEffect(() => {
+        NProgress.start()
+        NProgress.done()
+    },[location.pathname])
     return (
         <>
             <Header />
