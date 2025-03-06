f8762ded7c1ba86d30d5a54c6cba57b56b6f6111
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-03-06 11:40:17 +0700

    Search orders by keyword

--- src/modules/admin/pages/Orders/Orders.tsx
+++ src/modules/admin/pages/Orders/Orders.tsx
@@ -1,25 +1,37 @@
-import { Table } from "antd"
+import { Col, Row, Table, Input } from "antd"
 import orderColumns from "../../../../features/orders/order_columns"
 import { useDispatch, useSelector } from "react-redux"
 import { AppDispatch, RootState } from "../../../../common/types/store.type"
 import { useEffect, useState } from "react"
 import { fetchOrders } from "../../../../features/orders/orders.thunk"
 import Detail from "./Detail"
+import styles from "./Orders.module.scss";
 
-
+const {Search} = Input
 
 const Orders = () => {
     const [id, setId] = useState<string>('');
+    const [keyword, setKeyword] = useState<string>();
     const [openDetail, setOpenDetail] = useState<boolean>(false);
     const {items} = useSelector((state: RootState) => state.orders);
     const dispatch = useDispatch<AppDispatch>();
     useEffect(() => {
-        dispatch(fetchOrders({}))
-    },[dispatch])
+        dispatch(fetchOrders({
+            keyword
+        }))
+    },[dispatch, keyword])
 
     return (
         
         <>
+            <Row className={styles.orders__actions}>
+                <Col span={6}>
+                    <Search 
+                        placeholder="Nhập thông tin tìm kiếm..."
+                        onChange={(e) => setKeyword(e.target.value)}
+                    />
+                </Col>
+            </Row>
             <Table
                 columns={
                     orderColumns({
